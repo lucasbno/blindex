e3da3b51c5317752fdb74359bfd5a31a6292c781
0Zenix <zenixplays447@gmail.com>
2025-04-04 23:56:25 -0300

    Added sign_up_screen.dart

---
+++ lib/view/sign_up_view.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get_it/get_it.dart';
+
+
+class SignUpView extends StatefulWidget {
+  const SignUpView({super.key});
+
+  @override
+  State<SignUpView> createState() => _SignUpViewState();
+}
+
+class _SignUpViewState extends State<SignUpView> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return Center();
+  }
+}
