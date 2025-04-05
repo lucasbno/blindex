4868063ab1c3aec63c8aa4fa123a6587beb0e20f
0Zenix <zenixplays447@gmail.com>
2025-04-04 23:46:39 -0300

    Added home_view.dart placeholder

---
+++ lib/view/home_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  // TODO Replace with Home screen
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home Place Holder')),
+      body: Center(child: Text('W.I.P')),
+    );
+  }
+}
