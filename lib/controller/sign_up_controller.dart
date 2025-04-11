32abdd552c4c461d5d2ecd2f2afac5f9ab51f183
0Zenix <zenixplays447@gmail.com>
2025-04-11 17:21:21 -0300

    Created sign_up_controller.dart

---
+++ lib/controller/sign_up_controller.dart
@@ -0,0 +1,27 @@
+import 'package:blindex/model/user_model.dart';
+import 'package:blindex/repository/user_repository.dart';
+import 'package:flutter/material.dart';
+
+class SignUpController extends ChangeNotifier {
+
+  final UserRepository userRepository;
+
+  SignUpController({required this.userRepository});
+
+  bool signUp(String email, String password, String passwordConfirm) {
+    final users = userRepository.users;
+      
+    final userExists = users.any(
+      (u) => u != null && u.email == email,
+    );
+
+    if (userExists) return false;
+
+    if (password != passwordConfirm) return false;
+
+    final newUser = User(email: email, password: password);
+    users.add(newUser);
+
+    return true;
+  }
+}
