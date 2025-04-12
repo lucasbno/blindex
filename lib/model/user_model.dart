89be5039229141eccdc017f272ce0dad0345bbbe
0Zenix <zenixplays447@gmail.com>
2025-04-12 01:45:54 -0300

    Added missing fields to Sign Up View

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -1,22 +1,28 @@
 import 'package:bcrypt/bcrypt.dart';
 
 class User {
+  final String name;
+  final String phoneNumber;
   final String email;
   final String passwordHash;
 
-  User({required this.email, required String password})
+  User({required this.name, required this.email, required this.phoneNumber, required String password})
       : passwordHash = BCrypt.hashpw(password, BCrypt.gensalt());
 
   User copyWith({
+    String? name,
+    String? phoneNumber,
     String? email,
     String? passwordHash,
     }) {
       return User(
-      email: email ?? this.email, 
+      name: name ?? this.name,
+      phoneNumber: phoneNumber ?? this.phoneNumber,
+      email: email ?? this.email,
       password: passwordHash ?? this.passwordHash);
     }
 
   bool checkPassword(String password) {
-    return BCrypt.checkpw(password, passwordHash ?? '');
+    return BCrypt.checkpw(password, passwordHash);
   }
 }
