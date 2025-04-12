2e35fb4c9b8d2d744c298cea3ef9781999acc64e
0Zenix <zenixplays447@gmail.com>
2025-04-12 01:46:47 -0300

    Fixed user_repository.dart

--- lib/repository/user_repository.dart
+++ lib/repository/user_repository.dart
@@ -4,8 +4,8 @@ import '../model/user_model.dart';
 
 class UserRepository extends ChangeNotifier {
   List<User?> users = [
-    User(email: 'alice@example.com', password: 'password123'),
-    User(email: 'bob@example.com', password: 'hunter2')
+    User(name: 'Alice', phoneNumber: '(16) 0555-2368', email: 'alice@example.com', password: 'password123'),
+    User(name: 'Bob', phoneNumber: '(80) 0451-1138', email: 'bob@example.com', password: 'hunter2')
   ];
 
   void updateUser(User updatedUser) {
